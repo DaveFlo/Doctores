c8630be0751242f2eceaee6f16187e08d74e4798
DaVal117 <alex.fval11@gmail.com>
2018-02-14 10:25:16 -0600

    vava

--- www/js/maps.js
+++ www/js/maps.js
@@ -27,7 +27,7 @@ function deg2rad(deg) {
     			lato= 40.7127837;
     			longo=-74.00594130000002;
     		}
-    		console.log(lato+"nanananaasdn");
+    		
       
         };
 
@@ -40,6 +40,7 @@ function deg2rad(deg) {
        }
 
     navigator.geolocation.getCurrentPosition(onSuccess, onError); 
+    console.log(lato+"nanananaasdn");
   var map = new GMaps({
         div: '#map_canvas',
          width: '100%',
